a039bb98584292eb919d112ccfb7659915c7877b
chvck <chvckd@gmail.com>
2018-03-04 12:31:50 +0000

    Add handler docstrings

--- server/handler.go
+++ server/handler.go
@@ -2,12 +2,15 @@ package server
 
 import "github.com/chvck/meal-planner/controller"
 
+// Handler is a container for controllers, providing a single point of access
+// for the router
 type Handler struct {
 	controller.MenuController
 	controller.RecipeController
 	controller.UserController
 }
 
+// NewHandler creates a new Handler
 func NewHandler(mc controller.MenuController, rc controller.RecipeController, uc controller.UserController) *Handler {
 	return &Handler{mc, rc, uc}
 }
